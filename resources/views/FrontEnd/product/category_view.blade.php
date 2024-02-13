dd966d0e3f0a70a112b9ddecba0eb2b0e15dd026
Abu Sadek Chowdhury <92204065+sadek166621@users.noreply.github.com>
2024-02-13 18:36:01 +0600

    category view customize

--- resources/views/FrontEnd/product/category_view.blade.php
+++ resources/views/FrontEnd/product/category_view.blade.php
@@ -49,17 +49,17 @@
 {{--        <h2>Sale On Now</h2>--}}
 {{--        <hr>--}}
 <div id="product-container" class="row row-cols-2 row-cols-md-3 row-cols-lg-6 g-2">
-    @foreach ($products as $product_trending )
+    @foreach ($products as $product )
     <div class="col">
-        <?php $discountPercentage = round((($product_trending->regular_price - $product_trending->discount_price) / $product_trending->regular_price) * 100); ?>
+        <?php $discountPercentage = round((($product->regular_price - $product->discount_price) / $product->regular_price) * 100); ?>
         <div class="card h-100">
             <span class="favorite"><i class="fa-regular fa-heart"></i></span>
-            <img src="{{ asset($product_trending->product_thumbnail) }}" class="card-img-top" alt="...">
+            <img src="{{ asset($product->product_thumbnail) }}" class="card-img-top" alt="...">
             <div class="card-body">
-                <p class="product-text">{!! Str::substr($product_trending->name_en, 0, 20) !!}......</p>
-                <h5 class="product-price">৳{{ $product_trending->discount_price }}</h5>
+                <p class="product-text">{!! Str::substr($product->name_en, 0, 20) !!}......</p>
+                <h5 class="product-price">৳{{ $product->discount_price }}</h5>
                 <p class="discount-percent"><span
-                        class="discount-price">৳{{ $product_trending->regular_price }}</span> -
+                        class="discount-price">৳{{ $product->regular_price }}</span> -
                     {{ $discountPercentage }}%</p>
                 <small class="product-ratings">
                     <i class="fa-solid fa-star"></i>
@@ -67,19 +67,19 @@
                     <i class="fa-solid fa-star"></i>
                     <i class="fa-solid fa-star"></i>
                     <i class="fa-regular fa-star"></i>
-                    <i class="ratings">({{ $product_trending->stock_qty }})</i>
+                    <i class="ratings">({{ $product->stock_qty }})</i>
                 </small>
                 <div class="text-center">
                     <button type="button" class="buy_now">Buy Now</button>
-                    @if($product_trending->is_varient == 1)
-                    <button type="button" id="{{ $product_trending->id }}" onclick="productView(this.id)"
+                    @if($product->is_varient == 1)
+                    <button type="button" id="{{ $product->id }}" onclick="productView(this.id)"
                         data-bs-toggle="modal" data-bs-target="#quickViewModal" class="buy_now">Add to Cart</button>
                     @else
                     <input type="hidden" id="pfrom" value="direct">
-                    <input type="hidden" id="product_product_id" value="{{ $product_trending->id }}" min="1">
-                    <input type="hidden" id="{{ $product_trending->id }}-product_pname"
-                        value="{{ $product_trending->name_en }}">
-                    <button type="button" onclick="addToCartDirect({{ $product_trending->id }})" class="buy_now">Add
+                    <input type="hidden" id="product_product_id" value="{{ $product->id }}" min="1">
+                    <input type="hidden" id="{{ $product->id }}-product_pname"
+                        value="{{ $product->name_en }}">
+                    <button type="button" onclick="addToCartDirect({{ $product->id }})" class="buy_now">Add
                         to Cart</button>
                     @endif
 
@@ -346,7 +346,7 @@
     }
 
 </script>
-<script>
+{{-- <script>
     var offset = 12;
 
     $('#load-more-btn').click(function () {
@@ -368,31 +368,31 @@
                         // Append the new products to the container
                         $('#product-container').append(`
                         <div class="col">
-            <?php $discountPercentage = round((($product_trending->regular_price - $product_trending->discount_price) / $product_trending->regular_price) * 100); ?>
+            <?php $discountPercentage = round((($product->regular_price - $product->discount_price) / $product->regular_price) * 100); ?>
             <div class="card h-100">
                 <span class="favorite"><i class="fa-regular fa-heart"></i></span>
-                <img src="{{ asset($product_trending->product_thumbnail) }}" class="card-img-top" alt="...">
+                <img src="{{ asset($product->product_thumbnail) }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <p class="product-text">{!! Str::substr($product_trending->name_en, 0, 20) !!}......</p>
-                    <h5 class="product-price">৳{{ $product_trending->discount_price }}</h5>
-                    <p class="discount-percent"><span class="discount-price">৳{{ $product_trending->regular_price }}</span> - {{ $discountPercentage }}%</p>
+                    <p class="product-text">{!! Str::substr($product->name_en, 0, 20) !!}......</p>
+                    <h5 class="product-price">৳{{ $product->discount_price }}</h5>
+                    <p class="discount-percent"><span class="discount-price">৳{{ $product->regular_price }}</span> - {{ $discountPercentage }}%</p>
                     <small class="product-ratings">
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-regular fa-star"></i>
-                        <i class="ratings">({{ $product_trending->stock_qty }})</i>
+                        <i class="ratings">({{ $product->stock_qty }})</i>
                     </small>
                     <div class="text-center">
                         <button type="button" class="buy_now">Buy Now</button>
-                        @if($product_trending->is_varient == 1)
-                        <button type="button" id="{{ $product_trending->id }}" onclick="productView(this.id)" data-bs-toggle="modal" data-bs-target="#quickViewModal" class="buy_now">Add to Cart</button>
+                        @if($product->is_varient == 1)
+                        <button type="button" id="{{ $product->id }}" onclick="productView(this.id)" data-bs-toggle="modal" data-bs-target="#quickViewModal" class="buy_now">Add to Cart</button>
                         @else
                         <input type="hidden" id="pfrom" value="direct">
-                        <input type="hidden" id="product_product_id" value="{{ $product_trending->id }}"  min="1">
-                        <input type="hidden" id="{{ $product_trending->id }}-product_pname" value="{{ $product_trending->name_en }}">
-                        <button type="button" onclick="addToCartDirect({{ $product_trending->id }})" class="buy_now">Add to Cart</button>
+                        <input type="hidden" id="product_product_id" value="{{ $product->id }}"  min="1">
+                        <input type="hidden" id="{{ $product->id }}-product_pname" value="{{ $product->name_en }}">
+                        <button type="button" onclick="addToCartDirect({{ $product->id }})" class="buy_now">Add to Cart</button>
                         @endif
 
                     </div>
@@ -413,5 +413,5 @@
         });
     });
 
-</script>
+</script> --}}
 @endpush
